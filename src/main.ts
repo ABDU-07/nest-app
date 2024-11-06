fd78c9d490c4351047f72d13f08f877dfc6f60be
ABDU-07 <isamjonovabdurahmon23@gmail.com>
2024-11-06 13:03:55 +0500

    s

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/16.171.169.161/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/16.171.169.161/fullchain.pem'),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
