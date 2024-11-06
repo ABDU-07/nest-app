5d794c905e36a5483736c1bc5e06a4f8cda998e1
ABDU-07 <isamjonovabdurahmon23@gmail.com>
2024-11-06 19:22:42 +0500

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [  
-    UsersModule, 
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      database: 'postgres',
-      username: 'postgres',
-      password: '123456',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DB_HOST, 
+      port: +process.env.DB_PORT, 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadModels: true,
       synchronize: true,
     }),
